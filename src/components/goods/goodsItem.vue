4692df54167b9a7923ad7c56c9b5bde12ae5e824
Luo jiaxing <tastyluo@outlook.com>
2017-07-10 18:00:37 +0800

    修改配置

---
+++ src/components/goods/goodsItem.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="goods-item">
+    <div class="g-img">
+      <img :src="goodsImg" />
+      <div v-if="isSelfOperated">自营</div>
+    </div>
+    <div class="g-name">
+      <span>{{name}}</span>
+    </div>
+    <div class="g-price-goodrate">
+      <strong>
+        <em>¥</em>
+        <i>{{price}}</i>
+      </strong>
+      <strong>
+        <i>{{goodRate}}</i>
+        <em>%</em>
+      </strong>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import cpuImg from '@/assets/cpu.jpg'
+
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    shopName: {
+      type: String,
+      default: ''
+    },
+    price: {
+      type: Number,
+      default: null
+    },
+    goodRate: {
+      type: Number,
+      default: null
+    },
+    selfOperated: {
+      type: String,
+      default: '0'
+    },
+    link: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      goodsImg: cpuImg
+    }
+  },
+  computed: {
+    isSelfOperated () {
+      if (this.selfOperated === '0') {
+        return false
+      } else if (this.selfOperated === '1') {
+        return true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .goods-item {
+    width: 220px;
+    height: 300px;
+  }
+  .g-img {
+    width: 220px;
+    height: 220px;
+  }
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <a href="/"></a>
+    </div>
+    <div class="main-menu">
+      <ul>
+        <li>京东</li>
+        <li>淘宝</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    height: 40px;
+    width: 100%;
+    position: fixed;
+  }
+  .logo {
+    a {
+      background-image: url("./assets/logo.png")
+    }
+  }
+</style>
